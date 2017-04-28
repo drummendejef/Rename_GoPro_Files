d020005d06d01c26cc7c044896b88162209d8e1e
Joren <jorenvandamme@hotmail.com>
2017-04-28 14:46:34 +0200

    Project aangemaakt
    Console project aangemaakt mbv Visual Studio 2017

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rename_GoPro_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
