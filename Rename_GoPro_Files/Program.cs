36277929c08884dd07911e2e8c9267fdb90c3c5f
Joren <jorenvandamme@hotmail.com>
2017-05-02 13:15:43 +0200

    Better errorcatching
    - Added better "fill in path" test.
    - Added try catch

--- Rename_GoPro_Files/Program.cs
+++ Rename_GoPro_Files/Program.cs
@@ -17,11 +17,13 @@ namespace Rename_GoPro_Files
             Console.WriteLine("Enter path to folder that contains GoPro video's and press enter");
             string path = Console.ReadLine();//Read folderpath when user presses the enter key
 
-            if (path == string.Empty)//Check if user filled something in
+            while (path == string.Empty)
             {
                 Console.WriteLine("Please give a path");
+                path = Console.ReadLine();//Read folderpath when user presses the enter key
             }
-            else //User filled in the path
+
+            try
             {
                 //Get all files that contain "GP" or "GoPR" and are an .MP4 file. (GoPro files standard are like this, GOPR for the first file, GP for all the rest)
                 //Orderby LastWriteTime to look at the time they were created on the GoPro card itself.
@@ -30,10 +32,14 @@ namespace Rename_GoPro_Files
                 //Run over all files 
                 foreach (FileInfo file in allMP4filesinfolder)
                 {
-                    Console.WriteLine("Item " + counter + " : " + file.Name);//Print the files we are doing to show the user.
+                    Console.WriteLine("Item " + counter + " : " + file.Name);//Print the files we are converting to show the user.
                     File.Move(file.FullName, path + "/Part" + counter + "_" + file.Name);//Add File_number to each file in the right order.
                     counter++;
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.Message);
             }
 
 
