8a0e884871d8387b6eec2189e858dbdc9d6d6707
Joren <jorenvandamme@hotmail.com>
2017-05-16 11:21:13 +0200

    Adding GUI
    Adding a program with a GUI.
    It will also upload the video's to Stream the moment we get an API.
    Maybe I can do this for YouTube too.

---
+++ Rename_GoPro_Files/Rename_Upload_GoProFiles/ViewModel/GoProVM.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+
+namespace Rename_Upload_GoProFiles.ViewModel
+{
+    public class GoProVM : MainViewModel
+    {
+        #region Properties
+        public List<string> FoundItems { get; set; }
+        #endregion
+
+        //Constructor
+        public GoProVM()
+        {
+
+        }
+
+        #region ButtonCommands
+        public ICommand OpenFolderCommand
+        {
+            get { return new RelayCommand(OpenFolder); }
+        }
+
+
+        #endregion
+
+        #region Methods
+        private void OpenFolder()
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                //We openen een folder en slaan het pad van deze folder op.
+                DialogResult result = dialog.ShowDialog();
+                string folderPath = dialog.SelectedPath;
+
+                //We halen alle bestanden op uit deze map
+            }
+        }
+        #endregion
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //public void OnPropertyChanged(string name)
+        //{
+        //    if (this.PropertyChanged != null)
+        //        this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+
+        //}
+
+    }
+}
